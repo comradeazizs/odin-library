b6b3eaa2953bdee08d9c285650af2209cbf2501b
Aziz Shahmardanli <aziz.shahmardan@gmail.com>
2023-06-10 17:40:58 +0400

    add modal, remove button, grid

--- code.js
+++ code.js
@@ -4,14 +4,14 @@ let myLibrary = [
     author: "Jack London",
     pages: 354,
     read: false,
-    date: 1908
+    date: "1908"
   },
   {
     title: "Kürk Mantolu Madonna",
     author: "Sabahattin Ali",
     pages: "177",
     read: true,
-    date: 1943
+    date: "1943"
   }
 ];
 
@@ -28,11 +28,12 @@ function printCard(book) {
   card.classList.add("card");
 
   card.setHTML(
-    `<p>Book title: ${book.title}</p>
-    <p>Book author: ${book.author}</p>
-    <p> ${book.pages} pages</p>
+    `<p>Title: ${book.title}</p>
+    <p>By ${book.author}</p>
+    <p>${book.pages} pages</p>
     <p>${book.read ? "read" : "not read yet"}</p>
-    <p>Publish date: ${book.date}</p>
+    <p>Publish Date: ${book.date}</p>
+    <button type="button" class="remove btn">Remove</button>
     </div>`
   );
   shelf.appendChild(card);
@@ -42,6 +43,17 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function removeCardEvent() {
+  remove.forEach((button) =>
+    button.addEventListener("click", (e) => {
+      const nodesArray = [...document.querySelectorAll(".card")];
+      const index = nodesArray.indexOf(e.target.parentElement);
+      shelf.removeChild(e.target.parentElement);
+      myLibrary.splice(index, 1);
+    })
+  );
+}
+
 const shelf = document.querySelector(".shelf");
 const form = document.querySelector(".form-container");
 const title = document.querySelector("input[name=title]");
@@ -49,9 +61,24 @@ const author = document.querySelector("input[name=author]");
 const pages = document.querySelector("input[name=pages]");
 const date = document.querySelector("input[name=date]");
 const read = document.querySelector("input[name=read]");
-
 const addBtn = document.querySelector(".addBtn");
+const modal = document.querySelector(".modal");
+const modBtn = document.querySelector(".modalBtn");
+const modClose = document.querySelector(".close");
 
+modClose.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 addBtn.addEventListener("click", (event) => {
   event.preventDefault();
   addBookToLibrary(
@@ -61,7 +88,13 @@ addBtn.addEventListener("click", (event) => {
     shelf.removeChild(shelf.firstChild);
   }
   myLibrary.forEach(printCard);
+  remove = document.querySelectorAll(".remove");
+  removeCardEvent();
   form.reset();
+  modal.style.display = "none";
 });
 
 myLibrary.forEach(printCard);
+
+let remove = document.querySelectorAll(".remove");
+removeCardEvent();
